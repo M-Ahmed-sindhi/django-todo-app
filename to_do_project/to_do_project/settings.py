330b8448cf257fd608c1770948c4a08b82cf0a2c
MAC <mac@MACs-MacBook-Pro.local>
2026-01-05 19:30:37 +0500

    message

--- to_do_project/to_do_project/settings.py
+++ to_do_project/to_do_project/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import dj_database_url
+from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -62,12 +64,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'to_do_project.wsgi.application'
 
 # DATABASE (SQLite – OK for small apps on Render)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
@@ -85,8 +95,11 @@ USE_L10N = True
 USE_TZ = True
 
 # STATIC FILES
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR / 'static']
